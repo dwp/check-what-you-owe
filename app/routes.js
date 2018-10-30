3a466aa8f1082c938a27b7b333f5baa534c61cdd
Alex Colbeck <alex.colbeck.digitaldwp@gmail.com>
2018-10-30 16:56:15 +0000

    Creating conditional logic for eligibility questions

--- app/routes.js
+++ app/routes.js
@@ -8,19 +8,20 @@ router.get('/signout', function (req, res) {
 })
 
 // Branching
-router.post('/examples/branching/over-18-answer', function (req, res) {
+router.post('/04/eligibility/uc-credit-answer', function (req, res) {
   // Get the answer from session data
   // The name between the quotes is the same as the 'name' attribute on the input elements
   // However in JavaScript we can't use hyphens in variable names
 
-  let over18 = req.session.data['over-18']
+  let onUc = req.session.data['on-uc']
 
-  if (over18 === 'false') {
-    res.redirect('/docs/examples/branching/under-18')
+  if (onUc === 'false') {
+    res.redirect('/04/eligibility/off-uc')
   } else {
-    res.redirect('/docs/examples/branching/over-18')
+    res.redirect('/04/eligibility/on-uc')
   }
 })
+
 
 // Add your routes above the module.exports line
 module.exports = router
