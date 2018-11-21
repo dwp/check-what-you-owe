0d15a62c551bbf79725c1c5a1bf093d2415012cf
Alex Colbeck <alex.colbeck.digitaldwp@gmail.com>
2018-11-21 17:16:05 +0000

    Adding commits  to show Colin my mucky code

--- app/routes.js
+++ app/routes.js
@@ -2,40 +2,40 @@ const express = require('express')
 const fs = require('fs')
 const router = express.Router()
 
-// // Add your routes here - above the module.exports line
-// router.use(require('./middleware/locals'))
-//
-// // Data sources
-// router.all('/data/:data/source/:source', (req, res) => {
-//  const { data, source } = req.params
-//  res.json(require(`./data/${data}/source/${source}`))
-// })
-//
-// // Remove trailing slashes
-// router.all('\\S+/$', (req, res) => {
-//  res.redirect(301, req.path.slice(0, -1) + req.url.slice(req.path.length))
-// })
-//
-// // Find prototypes here
-// const search = `${__dirname}/views/prototypes/`
-// const prototypes = fs.readdirSync(search).filter(file => {
-//  return fs.statSync(`${search}/${file}`).isDirectory()
-// })
-//
-// // Multiple prototypes
-// for (const directory of prototypes) {
-//  const prototype = require(`${search}${directory}`)
-//   // Prototype static assets
-//  prototype.use(`/assets`, express.static(`${__dirname}/views/prototypes/${directory}/assets`))
-//   // Prototype router
-//  router.use(`/${directory}`, prototype)
-// }
-//
-// // Sign out clears session storage and goes back to start
-// router.get('/signout', function (req, res) {
-//   req.session.destroy()
-//   res.redirect('start')
-// })
+// Add your routes here - above the module.exports line
+router.use(require('./middleware/locals'))
+
+// Data sources
+router.all('/data/:data/source/:source', (req, res) => {
+ const { data, source } = req.params
+ res.json(require(`./data/${data}/source/${source}`))
+})
+
+// Remove trailing slashes
+router.all('\\S+/$', (req, res) => {
+ res.redirect(301, req.path.slice(0, -1) + req.url.slice(req.path.length))
+})
+
+// Find prototypes here
+const search = `${__dirname}/views/prototypes/`
+const prototypes = fs.readdirSync(search).filter(file => {
+ return fs.statSync(`${search}/${file}`).isDirectory()
+})
+
+// Multiple prototypes
+for (const directory of prototypes) {
+ const prototype = require(`${search}${directory}`)
+  // Prototype static assets
+ prototype.use(`/assets`, express.static(`${__dirname}/views/prototypes/${directory}/assets`))
+  // Prototype router
+ router.use(`/${directory}`, prototype)
+}
+
+// Sign out clears session storage and goes back to start
+router.get('/signout', function (req, res) {
+  req.session.destroy()
+  res.redirect('start')
+})
 
 // Branching for eligibility questions on 05 prototype
 router.post('/05/eligibility/universal-credit-answer', function (req, res) {
