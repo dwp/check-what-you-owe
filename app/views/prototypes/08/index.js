eb2f6f3ea3f82c410a2bc857ef433fd94089597d
Alex Colbeck <alex.colbeck.digitaldwp@gmail.com>
2018-12-14 16:51:18 +0000

    Adding routing into own directory

--- app/views/prototypes/08/index.js
+++ app/views/prototypes/08/index.js
@@ -16,4 +16,30 @@ router.all('/:view', (req, res) => {
   res.render(`${__dirname}/views/${req.params.view}`)
 })
 
+// 08 prototype routing
+
+// Making an addition payment
+router.post('/views/additional-payment/additional-payment-calculator', function (req, res) {
+  const submitted = req.session.data;
+
+  // Format answer as whole number
+  const answer = parseFloat(submitted['additional-payment'] || 0)
+
+  if (answer <= 100) {
+    res.redirect('/prototypes/08/views/additional-payment/small')
+  }
+
+  if (answer <= 250) {
+    res.redirect('/prototypes/08/views/additional-payment/medium')
+  }
+
+  if (answer >= 251) {
+    res.redirect('/prototypes/08/views/additional-payment/large')
+  }
+})
+
+// router.all('/views/additional-payment/additional-payment-calculator', (req, res) => {
+//   return res.send('Anything??')
+// })
+
 module.exports = router
