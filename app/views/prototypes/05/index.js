843beb960954f19fc3a6d61f688c321ab09d53d7
Alex Colbeck <alex.colbeck.digitaldwp@gmail.com>
2019-01-10 07:58:32 +0000

    Fixing broking routing in prototype 05

--- app/views/prototypes/05/index.js
+++ app/views/prototypes/05/index.js
@@ -16,8 +16,8 @@ router.all('/:view', (req, res) => {
   res.render(`${__dirname}/views/${req.params.view}`)
 })
 
-// checking whether user is in UC
-router.post('/prototypes/05/views/eligibility', function (req, res) {
+//check if user is on UC
+router.post('/views/eligibility', function (req, res) {
   const submitted = req.session.data;
 
   if (submitted['universal-credit'] === 'on-uc-yes') {
@@ -29,34 +29,46 @@ router.post('/prototypes/05/views/eligibility', function (req, res) {
   }
 })
 
-// Branching for eligibility questions on 05 prototype
-router.post('prototypes/05/views/eligibility/what-would-you-like-to-do-answer', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
 
-  let WhatWouldYouLikeToDo = req.session.data['what-would-you-like-to-do']
-
-  if (WhatWouldYouLikeToDo === 'false') {
-    res.redirect('prototypes/05/views/eligibility/check-what-you-owe')
-  } else {
-    res.redirect('prototypes/05/views/eligibility/pay-what-you-owe')
-  }
-})
-
-// Branching for eligibility questions on 05 prototype
-router.post('prototypes/05/views/eligibility/repayment-answer', function (req, res) {
-  let RepaymentOptions = req.session.data['repayment-options']
-
-  if (RepaymentOptions === 'full') {
-    res.redirect('prototypes/05/views/eligibility/full')
-  }
-  if (RepaymentOptions === 'partial') {
-    res.redirect('prototypes/05/views/eligibility/partial')
-  }
-  if (RepaymentOptions === 'plan') {
-    res.redirect('prototypes/05/views/eligibility/plan')
-  }
-})
+// // Branching for eligibility questions on 05 prototype
+// router.post('/views/eligibility/off-universal-credit', function (req, res) {
+//   const submitted = req.session.data;
+//
+//   if (submitted['what-would-you-like-to-do'] === 'check') {
+//     res.redirect('/prototypes/05/views/eligibility/on-universal-credit')
+//   }
+//
+//   if (submitted['what-would-you-like-to-do'] === 'repay') {
+//     res.redirect('/prototypes/05/views/eligibility/off-universal-credit')
+//   }
+// })
+//
+//
+// // Branching for eligibility questions on 05 prototype
+// router.post('prototypes/05/views/eligibility/what-would-you-like-to-do-answer', function (req, res) {
+//
+//   let WhatWouldYouLikeToDo = req.session.data['what-would-you-like-to-do']
+//
+//   if (WhatWouldYouLikeToDo === 'false') {
+//     res.redirect('prototypes/05/views/eligibility/check-what-you-owe')
+//   } else {
+//     res.redirect('prototypes/05/views/eligibility/pay-what-you-owe')
+//   }
+// })
+//
+// // Branching for eligibility questions on 05 prototype
+// router.post('prototypes/05/views/eligibility/repayment-answer', function (req, res) {
+//   let RepaymentOptions = req.session.data['repayment-options']
+//
+//   if (RepaymentOptions === 'full') {
+//     res.redirect('prototypes/05/views/eligibility/full')
+//   }
+//   if (RepaymentOptions === 'partial') {
+//     res.redirect('prototypes/05/views/eligibility/partial')
+//   }
+//   if (RepaymentOptions === 'plan') {
+//     res.redirect('prototypes/05/views/eligibility/plan')
+//   }
+// })
 
 module.exports = router
