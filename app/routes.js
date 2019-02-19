0e825eeef3cba764429a10205218b1e60225f9b4
Alex Colbeck <alex.colbeck.digitaldwp@gmail.com>
2019-02-19 16:02:23 +0000

    Adding new todays date code

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,74 @@
 const express = require('express')
 const fs = require('fs')
 const router = express.Router()
+const {format} = require('date-fns') // ------------------ Require format function from date-fns
+
+
+
+// Create a new middleware function.
+// This makes it run on every page load in the application
+router.use('/', (req, res, next) => {
+  // Get todays date as a JS date
+  const today = new Date()
+
+  // Set a local (a variable available in the .html files) using the format function from date-fns
+  res.locals.todayDateFNS = format(today, 'D MMMM YYYY - hh:mm:ss')
+
+  // Set a local (a variable available in the .html files) using the format function from date-fns
+  res.locals.currentMonthFNS = format(today, 'MMMM')
+
+  // Set a local (a variable available in the .html files) using the format function from date-fns
+  res.locals.currentYearFNS = format(today, 'YYYY')
+
+  // Tell express we are done with this middleware function
+  next()
+})
+
+
+
+// Create a new middleware function.
+// This makes it run on every page load in the application
+router.use('/', (req, res, next) => {
+  // Get todays date as a JS date
+  const today = new Date();
+
+  // Get the day from the today variable
+  const day = today.getDate();
+
+  // Create an array of months as words
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
+
+  // Get the month in words from the months array
+  // by using the month as a number from the today variable
+  const month = months[today.getMonth()];
+
+  // Get the year from the today variable
+  const year = today.getFullYear();
+
+  // Create the time using the hours, minutes and seconds from the today variable
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds();
+
+  // Set a local (a variable available in the .html files) using the variables we just created
+  res.locals.todayDateJS = `${day} ${month} ${year} - ${hours}:${minutes}:${seconds}`
+
+  // Tell express we are done with this middleware function
+  next()
+})
 
 // Add your routes here - above the module.exports line
 router.use(require('./middleware/locals'))
