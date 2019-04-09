b8946aef88d0ee122a8f706a2ed34781fed508a3
Dominic Norris <you@example.com>
2019-04-09 09:30:01 +0100

    Updating MVP2 content and adding elements of hmcts prototyping kit

--- server.js
+++ server.js
@@ -350,6 +350,10 @@ app.use(function (err, req, res, next) {
   res.send(err.message)
 })
 
+//HMCTS prototype Kit
+app.use('/assets', express.static(path.join(__dirname, '/node_modules/@hmcts/frontend/assets')));
+app.use('/node_modules/hmcts-frontend', express.static(path.join(__dirname, '/node_modules/@hmcts/frontend')))
+
 console.log('\nGOV.UK Prototype Kit v' + releaseVersion)
 console.log('\nNOTICE: the kit is for building prototypes, do not use it for production services.')
 
